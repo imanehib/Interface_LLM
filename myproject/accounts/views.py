f7705d561119eb45188cc9caede07ca40ad51a3b
hibaoui <imane.hibaoui@etu.u-paris.fr>
2025-04-09 10:42:10 +0200

    Creation de formulaire personalisée pour les étudiants et les professeurs

--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -1,10 +1,14 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render, redirect
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login, get_user_model
+from .forms import StudentSignUpForm, ProfessorSignUpForm
+
+# Récupère le modèle utilisateur personnalisé
+CustomUser = get_user_model()
 
 def index(request):
     return render(request, 'index.html')
+
 
 # Vue de connexion (Login)
 class CustomLoginView(LoginView):
@@ -16,19 +20,51 @@ class CustomLoginView(LoginView):
         context['role'] = self.request.GET.get('role', '')
         return context
 
+
 # Vue de déconnexion (Logout)
 class CustomLogoutView(LogoutView):
     next_page = '/'  # Redirige vers la page d'accueil après la déconnexion
 
-# Vue d'inscription (Signup)
-def signup(request):
+
+# Vue d'inscription pour étudiants
+def student_signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = StudentSignUpForm(request.POST)
         if form.is_valid():
-            user = form.save()
-            # Connecter l'utilisateur après inscription
+            user = form.save(commit=False)
+            # Assigner le rôle étudiant
+            user.role = CustomUser.STUDENT  # Ou 'student' si vous avez défini des chaînes
+            user.save()
             login(request, user)
-            return redirect('home')  # Redirige vers la page d'accueil (ou une page dédiée)
+            return redirect('text_analysis:home')  # Redirige vers le correcteur de texte ou une autre page
     else:
-        form = UserCreationForm()
-    return render(request, 'registration/signup.html', {'form': form})
+        form = StudentSignUpForm()
+    return render(request, 'registration/student_signup.html', {'form': form})
+
+
+# Vue d'inscription pour professeurs
+def professor_signup(request):
+    if request.method == 'POST':
+        form = ProfessorSignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            # Assigner le rôle professeur
+            user.role = CustomUser.PROFESSOR  # Ou 'professor'
+            user.save()
+            login(request, user)
+            return redirect('text_analysis:home')
+    else:
+        form = ProfessorSignUpForm()
+    return render(request, 'registration/professor_signup.html', {'form': form})
+
+
+# Vue d'inscription générale (si vous souhaitez garder une vue d'inscription générique)
+# Cette vue peut rediriger l'utilisateur vers la page d'inscription appropriée en fonction d'un paramètre GET, par exemple.
+def signup(request):
+    # Si un paramètre 'role' est passé dans l'URL pour différencier le type
+    role = request.GET.get('role', '')
+    if role == 'professor':
+        return professor_signup(request)
+    else:
+        # Par défaut, on redirige vers l'inscription étudiant
+        return student_signup(request)
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -122,9 +122,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
-TIME_ZONE = 'Europe/Paris'  # Exemple pour la France
+TIME_ZONE = 'Europe/Paris'  
 
 USE_I18N = True
 
