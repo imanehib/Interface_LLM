cce5f4ae002a2e0448fbb9782e87790488c3d4ba
hibaoui <imane.hibaoui@etu.u-paris.fr>
2025-04-09 10:59:10 +0200

    Mise à jour du lien d'inscription dans index pour utiliser accounts:signup_choice

--- myproject/accounts/urls.py
+++ myproject/accounts/urls.py
@@ -1,10 +1,18 @@
 from django.urls import path
-from .views import CustomLoginView, CustomLogoutView, signup
+from .views import (
+    CustomLoginView,
+    CustomLogoutView,
+    signup_choice,
+    student_signup,
+    professor_signup
+)
 
 app_name = 'accounts'
 
 urlpatterns = [
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('logout/', CustomLogoutView.as_view(), name='logout'),
-    path('signup/', signup, name='signup'),
+    path('login/', CustomLoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', CustomLogoutView.as_view(next_page='/'), name='logout'),
+    path('signup/', signup_choice, name='signup_choice'),
+    path('signup/student/', student_signup, name='student_signup'),
+    path('signup/professor/', professor_signup, name='professor_signup'),
 ]
--- myproject/accounts/views.py
+++ myproject/accounts/views.py
@@ -9,22 +9,19 @@ CustomUser = get_user_model()
 def index(request):
     return render(request, 'index.html')
 
-
 # Vue de connexion (Login)
 class CustomLoginView(LoginView):
     template_name = 'registration/login.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # Par exemple, on peut transmettre un paramètre "role" pour adapter l'affichage
+        # Vous pouvez transmettre un paramètre "role" si nécessaire
         context['role'] = self.request.GET.get('role', '')
         return context
 
-
 # Vue de déconnexion (Logout)
 class CustomLogoutView(LogoutView):
-    next_page = '/'  # Redirige vers la page d'accueil après la déconnexion
-
+    next_page = '/'  # Redirige vers la page d'accueil après déconnexion
 
 # Vue d'inscription pour étudiants
 def student_signup(request):
@@ -32,15 +29,13 @@ def student_signup(request):
         form = StudentSignUpForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            # Assigner le rôle étudiant
-            user.role = CustomUser.STUDENT  # Ou 'student' si vous avez défini des chaînes
+            user.role = CustomUser.STUDENT  # ou simplement 'student'
             user.save()
             login(request, user)
-            return redirect('text_analysis:home')  # Redirige vers le correcteur de texte ou une autre page
+            return redirect('text_analysis:home')  # ou une autre route adaptée
     else:
         form = StudentSignUpForm()
     return render(request, 'registration/student_signup.html', {'form': form})
-
 
 # Vue d'inscription pour professeurs
 def professor_signup(request):
@@ -48,8 +43,7 @@ def professor_signup(request):
         form = ProfessorSignUpForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            # Assigner le rôle professeur
-            user.role = CustomUser.PROFESSOR  # Ou 'professor'
+            user.role = CustomUser.PROFESSOR  # ou simplement 'professor'
             user.save()
             login(request, user)
             return redirect('text_analysis:home')
@@ -57,14 +51,6 @@ def professor_signup(request):
         form = ProfessorSignUpForm()
     return render(request, 'registration/professor_signup.html', {'form': form})
 
-
-# Vue d'inscription générale (si vous souhaitez garder une vue d'inscription générique)
-# Cette vue peut rediriger l'utilisateur vers la page d'inscription appropriée en fonction d'un paramètre GET, par exemple.
-def signup(request):
-    # Si un paramètre 'role' est passé dans l'URL pour différencier le type
-    role = request.GET.get('role', '')
-    if role == 'professor':
-        return professor_signup(request)
-    else:
-        # Par défaut, on redirige vers l'inscription étudiant
-        return student_signup(request)
+# Nouvelle vue qui affiche le choix : étudiant ou professeur
+def signup_choice(request):
+    return render(request, 'registration/signup_choice.html')
